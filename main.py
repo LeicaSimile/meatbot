dec6bc6a5fc89ee1e5e861651573f9df69527d74
Tum <tummytum3@gmail.com>
2016-07-22 23:05:03 -0400

    -meatbot.py (redundant)

--- main.py
+++ main.py
@@ -7,15 +7,7 @@ class MeatBot(irc.IrcBot):
 
     def join(self, channel, msg=""):
         pass
-
-    def update(self, files=None):
-        """ Reads files again. """
-        if files:
-            ## Read those in the list only.
-            pass
-        else:
-            ## Read all files.
-            pass
+    
 
 
 def main():
