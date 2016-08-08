807d554f559913988a6c32760670935700d48798
Tum <tummytum3@gmail.com>
2016-08-07 22:05:58 -0400

    Add server numeric event handler functions
    Add numeric codes to handlers dict in IrcBot().process_line and corresponding functions to handle the numerics.

--- irc.py
+++ irc.py
@@ -405,13 +405,155 @@ class IrcBot(threading.Thread):
             "PRIVMSG": self.on_privmsg,
             "QUIT": self.on_quit,
             "TOPIC": self.on_topic,
+            "001": self.on_rpl_welcome,  # "RPL_WELCOME"
+            "002": self.on_rpl_yourhost,  # "RPL_YOURHOST"
+            "003": self.on_rpl_created,  # "RPL_CREATED"
+            "004": self.on_rpl_myinfo,  # "RPL_MYINFO"
+            "005": self.on_rpl_isupport,  # "RPL_ISUPPORT"
+            "008": self.on_rpl_snomask,  # "RPL_SNOMASK"
+            "009": self.on_rpl_statmemtot,  # "RPL_STATMEMTOT"
+            "010": self.on_rpl_bounce,  # "RPL_BOUNCE"
+            "200": self.on_rpl_tracelink,  # "RPL_TRACELINK"
+            "201": self.on_rpl_traceconnecting,  # "RPL_TRACECONNECTING"
+            "202": self.on_rpl_tracehandshake,  # "RPL_TRACEHANDSHAKE"
+            "203": self.on_rpl_traceunknown,  # "RPL_TRACEUNKNOWN"
+            "204": self.on_rpl_traceoperator,  # "RPL_TRACEOPERATOR"
+            "205": self.on_rpl_traceuser,  # "RPL_TRACEUSER"
+            "206": self.on_rpl_traceserver,  # "RPL_TRACESERVER"
+            "208": self.on_rpl_tracenewtype,  # "RPL_TRACENEWTYPE"
+            "209": self.on_rpl_traceclass,  # "RPL_TRACECLASS"
+            "211": self.on_rpl_statslinkinfo,  # "RPL_STATSLINKINFO"
+            "212": self.on_rpl_statscommands,  # "RPL_STATSCOMMANDS"
+            "213": self.on_rpl_statscline,  # "RPL_STATSCLINE"
+            "214": self.on_rpl_statsnline,  # "RPL_STATSNLINE"
+            "215": self.on_rpl_statsiline,  # "RPL_STATSILINE"
+            "216": self.on_rpl_statskline,  # RPL_STATSKLINE
+            "217": self.on_rpl_statsqline,  # RPL_STATSQLINE
+            "218": self.on_rpl_statsyline,  # RPL_STATSYLINE
+            "219": self.on_rpl_endofstats,  # RPL_ENDOFSTATS
+            "231": self.on_rpl_serviceinfo,  # RPL_SERVICEINFO
+            "232": self.on_rpl_endofservices,  # RPL_ENDOFSERVICES
+            "233": self.on_rpl_service,  # RPL_SERVICE
+            "234": self.on_rpl_servlist,  # RPL_SERVLIST
+            "235": self.on_rpl_servlistend,  # RPL_SERVLISTEND
+            "241": self.on_rpl_statslline,  # RPL_STATSLLINE
+            "242": self.on_rpl_statsuptime,  # RPL_STATSUPTIME
+            "243": self.on_rpl_statsoline,  # RPL_STATSOLINE
+            "244": self.on_rpl_statshline,  # RPL_STATSHLINE
+            "221": self.on_rpl_umodeis,  # RPL_UMODEIS
+            "251": self.on_rpl_luserclient,  # RPL_LUSERCLIENT
+            "252": self.on_rpl_luserop,  # RPL_LUSEROP
+            "253": self.on_rpl_luserunknown,  # RPL_LUSERUNKNOWN
+            "254": self.on_rpl_luserchannels,  # RPL_LUSERCHANNELS
+            "255": self.on_rpl_luserme,  # RPL_LUSERME
+            "256": self.on_rpl_adminme,  # RPL_ADMINME
+            "257": self.on_rpl_adminloc1,  # RPL_ADMINLOC1
+            "258": self.on_rpl_adminloc2,  # RPL_ADMINLOC2
+            "259": self.on_rpl_adminemail,  # RPL_ADMINEMAIL
+            "261": self.on_rpl_tracelog,  # RPL_TRACELOG
+            "300": self.on_rpl_none,  # RPL_NONE
+            "301": self.on_rpl_away,  # RPL_AWAY
+            "302": self.on_rpl_userhost,  # RPL_USERHOST
+            "303": self.on_rpl_ison,  # RPL_ISON
+            "305": self.on_rpl_unaway,  # RPL_UNAWAY
+            "306": self.on_rpl_nowaway,  # RPL_NOWAWAY
+            "311": self.on_rpl_whoisuser,  # RPL_WHOISUSER
+            "312": self.on_rpl_whoisserver,  # RPL_WHOISSERVER
+            "313": self.on_rpl_whoisoperator,  # RPL_WHOISOPERATOR
+            "314": self.on_rpl_whowasuser,  # RPL_WHOWASUSER
+            "315": self.on_rpl_endofwho,  # RPL_ENDOFWHO
+            "316": self.on_rpl_whoischanop,  # RPL_WHOISCHANOP
+            "317": self.on_rpl_whoisidle,  # RPL_WHOISIDLE
+            "318": self.on_rpl_endofwhois,  # RPL_ENDOFWHOIS
+            "319": self.on_rpl_whoischannels,  # RPL_WHOISCHANNELS
+            "321": self.on_rpl_liststart,  # RPL_LISTSTART
+            "322": self.on_rpl_list,  # RPL_LIST
+            "323": self.on_rpl_listend,  # RPL_LISTEND
+            "324": self.on_rpl_channelmodeis,  # RPL_CHANNELMODEIS
+            "331": self.on_rpl_notopic,  # RPL_NOTOPIC
+            "332": self.on_rpl_topic,  # RPL_TOPIC
+            "341": self.on_rpl_inviting,  # RPL_INVITING
+            "342": self.on_rpl_summoning,  # RPL_SUMMONING
+            "351": self.on_rpl_version,  # RPL_VERSION
+            "352": self.on_rpl_whoreply,  # RPL_WHOREPLY
+            "353": self.on_rpl_namreply,  # RPL_NAMREPLY
+            "361": self.on_rpl_killdone,  # RPL_KILLDONE
+            "362": self.on_rpl_closing,  # RPL_CLOSING
+            "363": self.on_rpl_closeend,  # RPL_CLOSEEND
+            "366": self.on_rpl_endofnames,  # RPL_ENDOFNAMES
+            "364": self.on_rpl_links,  # RPL_LINKS
+            "365": self.on_rpl_endoflinks,  # RPL_ENDOFLINKS
+            "367": self.on_rpl_banlist,  # RPL_BANLIST
+            "368": self.on_rpl_endofbanlist,  # RPL_ENDOFBANLIST
+            "369": self.on_rpl_endofwhowas,  # RPL_ENDOFWHOWAS
+            "371": self.on_rpl_info,  # RPL_INFO
+            "372": self.on_rpl_motd,  # RPL_MOTD
+            "373": self.on_rpl_infostart,  # RPL_INFOSTART
+            "374": self.on_rpl_endofinfo,  # RPL_ENDOFINFO
+            "375": self.on_rpl_motdstart,  # RPL_MOTDSTART
+            "376": self.on_rpl_endofmotd,  # RPL_ENDOFMOTD
+            "381": self.on_rpl_youreoper,  # RPL_YOUREOPER
+            "382": self.on_rpl_rehashing,  # RPL_REHASHING
+            "384": self.on_rpl_myportis,  # RPL_MYPORTIS
+            "391": self.on_rpl_time,  # RPL_TIME
+            "392": self.on_rpl_usersstart,  # RPL_USERSSTART
+            "393": self.on_rpl_users,  # RPL_USERS
+            "394": self.on_rpl_endofusers,  # RPL_ENDOFUSERS
+            "395": self.on_rpl_nousers,  # RPL_NOUSERS
+            "401": self.on_err_nosuchnick,  # ERR_NOSUCHNICK
+            "402": self.on_err_nosuchserver,  # ERR_NOSUCHSERVER
+            "403": self.on_err_nosuchchannel,  # ERR_NOSUCHCHANNEL
+            "404": self.on_err_cannotsendtochan,  # ERR_CANNOTSENDTOCHAN
+            "405": self.on_err_toomanychannels,  # ERR_TOOMANYCHANNELS
+            "406": self.on_err_wasnosuchnick,  # ERR_WASNOSUCHNICK
+            "407": self.on_err_toomanytargets,  # ERR_TOOMANYTARGETS
+            "409": self.on_err_noorigin,  # ERR_NOORIGIN
+            "411": self.on_err_norecipient,  # ERR_NORECIPIENT
+            "412": self.on_err_notexttosend,  # ERR_NOTEXTTOSEND
+            "413": self.on_err_notoplevel,  # ERR_NOTOPLEVEL
+            "414": self.on_err_wildtoplevel,  # ERR_WILDTOPLEVEL
+            "421": self.on_err_unknowncommand,  # ERR_UNKNOWNCOMMAND
+            "422": self.on_err_nomotd,  # ERR_NOMOTD
+            "423": self.on_err_noadmininfo,  # ERR_NOADMININFO
+            "424": self.on_err_fileerror,  # ERR_FILEERROR
+            "431": self.on_err_nonicknamegiven,  # ERR_NONICKNAMEGIVEN
+            "432": self.on_err_erroneusnickname,  # ERR_ERRONEUSNICKNAME
+            "433": self.on_err_nicknameinuse,  # ERR_NICKNAMEINUSE
+            "436": self.on_err_nickcollision,  # ERR_NICKCOLLISION
+            "441": self.on_err_usernotinchannel,  # ERR_USERNOTINCHANNEL
+            "442": self.on_err_notonchannel,  # ERR_NOTONCHANNEL
+            "443": self.on_err_useronchannel,  # ERR_USERONCHANNEL
+            "444": self.on_err_nologin,  # ERR_NOLOGIN
+            "445": self.on_err_summondisabled,  # ERR_SUMMONDISABLED
+            "446": self.on_err_usersdisabled,  # ERR_USERSDISABLED
+            "451": self.on_err_notregistered,  # ERR_NOTREGISTERED
+            "461": self.on_err_needmoreparams,  # ERR_NEEDMOREPARAMS
+            "462": self.on_err_alreadyregistred,  # ERR_ALREADYREGISTRED
+            "463": self.on_err_nopermforhost,  # ERR_NOPERMFORHOST
+            "464": self.on_err_passwdmismatch,  # ERR_PASSWDMISMATCH
+            "465": self.on_err_yourebannedcreep,  # ERR_YOUREBANNEDCREEP
+            "466": self.on_err_youwillbebanned,  # ERR_YOUWILLBEBANNED
+            "467": self.on_err_keyset,  # ERR_KEYSET
+            "471": self.on_err_channelisfull,  # ERR_CHANNELISFULL
+            "472": self.on_err_unknownmode,  # ERR_UNKNOWNMODE
+            "473": self.on_err_inviteonlychan,  # ERR_INVITEONLYCHAN
+            "474": self.on_err_bannedfromchan,  # ERR_BANNEDFROMCHAN
+            "475": self.on_err_badchannelkey,  # ERR_BADCHANNELKEY
+            "476": self.on_err_badchanmask,  # ERR_BADCHANMASK
+            "481": self.on_err_noprivileges,  # ERR_NOPRIVILEGES
+            "482": self.on_err_chanoprivsneeded,  # ERR_CHANOPRIVSNEEDED
+            "483": self.on_err_cantkillserver,  # ERR_CANTKILLSERVER
+            "491": self.on_err_nooperhost,  # ERR_NOOPERHOST
+            "492": self.on_err_noservicehost,  # ERR_NOSERVICEHOST
+            "501": self.on_err_umodeunknownflag,  # ERR_UMODEUNKNOWNFLAG
+            "502": self.on_err_usersdontmatch,  # ERR_USERSDONTMATCH
             }
         
         line = IrcMessage(line, time.time())
         if line.command in handlers:
             handlers[line.command](line)
 
-        print(line)
+        print(line.rawMsg)
 
     def raw_send(self, msg, output=None):
         if output is None:
@@ -492,6 +634,433 @@ class IrcBot(threading.Thread):
         pass
 
     def on_topic(self, msg):
+        pass
+
+    ## Server numeric event handlers:
+    def on_rpl_welcome(self, msg):
+        pass
+
+    def on_rpl_yourhost(self, msg):
+        pass
+
+    def on_rpl_created(self, msg):
+        pass
+
+    def on_rpl_myinfo(self, msg):
+        pass
+
+    def on_rpl_isupport(self, msg):
+        pass
+
+    def on_rpl_snomask(self, msg):
+        pass
+
+    def on_rpl_statmemtot(self, msg):
+        pass
+
+    def on_rpl_bounce(self, msg):
+        pass
+
+    def on_rpl_tracelink(self, msg):
+        pass
+
+    def on_rpl_traceconnecting(self, msg):
+        pass
+
+    def on_rpl_tracehandshake(self, msg):
+        pass
+
+    def on_rpl_traceunknown(self, msg):
+        pass
+
+    def on_rpl_traceoperator(self, msg):
+        pass
+
+    def on_rpl_traceuser(self, msg):
+        pass
+
+    def on_rpl_traceserver(self, msg):
+        pass
+
+    def on_rpl_tracenewtype(self, msg):
+        pass
+
+    def on_rpl_traceclass(self, msg):
+        pass
+
+    def on_rpl_statslinkinfo(self, msg):
+        pass
+
+    def on_rpl_statscommands(self, msg):
+        pass
+
+    def on_rpl_statscline(self, msg):
+        pass
+
+    def on_rpl_statsnline(self, msg):
+        pass
+
+    def on_rpl_statsiline(self, msg):
+        pass
+
+    def on_rpl_statskline(self, msg):
+        pass
+
+    def on_rpl_statsqline(self, msg):
+        pass
+
+    def on_rpl_statsyline(self, msg):
+        pass
+
+    def on_rpl_endofstats(self, msg):
+        pass
+
+    def on_rpl_serviceinfo(self, msg):
+        pass
+
+    def on_rpl_endofservices(self, msg):
+        pass
+
+    def on_rpl_service(self, msg):
+        pass
+
+    def on_rpl_servlist(self, msg):
+        pass
+
+    def on_rpl_servlistend(self, msg):
+        pass
+
+    def on_rpl_statslline(self, msg):
+        pass
+
+    def on_rpl_statsuptime(self, msg):
+        pass
+
+    def on_rpl_statsoline(self, msg):
+        pass
+
+    def on_rpl_statshline(self, msg):
+        pass
+
+    def on_rpl_umodeis(self, msg):
+        pass
+
+    def on_rpl_luserclient(self, msg):
+        pass
+
+    def on_rpl_luserop(self, msg):
+        pass
+
+    def on_rpl_luserunknown(self, msg):
+        pass
+
+    def on_rpl_luserchannels(self, msg):
+        pass
+
+    def on_rpl_luserme(self, msg):
+        pass
+
+    def on_rpl_adminme(self, msg):
+        pass
+
+    def on_rpl_adminloc1(self, msg):
+        pass
+
+    def on_rpl_adminloc2(self, msg):
+        pass
+
+    def on_rpl_adminemail(self, msg):
+        pass
+
+    def on_rpl_tracelog(self, msg):
+        pass
+
+    def on_rpl_none(self, msg):
+        pass
+
+    def on_rpl_away(self, msg):
+        pass
+
+    def on_rpl_userhost(self, msg):
+        pass
+
+    def on_rpl_ison(self, msg):
+        pass
+
+    def on_rpl_unaway(self, msg):
+        pass
+
+    def on_rpl_nowaway(self, msg):
+        pass
+
+    def on_rpl_whoisuser(self, msg):
+        pass
+
+    def on_rpl_whoisserver(self, msg):
+        pass
+
+    def on_rpl_whoisoperator(self, msg):
+        pass
+
+    def on_rpl_whowasuser(self, msg):
+        pass
+
+    def on_rpl_endofwho(self, msg):
+        pass
+
+    def on_rpl_whoischanop(self, msg):
+        pass
+
+    def on_rpl_whoisidle(self, msg):
+        pass
+
+    def on_rpl_endofwhois(self, msg):
+        pass
+
+    def on_rpl_whoischannels(self, msg):
+        pass
+
+    def on_rpl_liststart(self, msg):
+        pass
+
+    def on_rpl_list(self, msg):
+        pass
+
+    def on_rpl_listend(self, msg):
+        pass
+
+    def on_rpl_channelmodeis(self, msg):
+        pass
+
+    def on_rpl_notopic(self, msg):
+        pass
+
+    def on_rpl_topic(self, msg):
+        pass
+
+    def on_rpl_inviting(self, msg):
+        pass
+
+    def on_rpl_summoning(self, msg):
+        pass
+
+    def on_rpl_version(self, msg):
+        pass
+
+    def on_rpl_whoreply(self, msg):
+        pass
+
+    def on_rpl_namreply(self, msg):
+        pass
+
+    def on_rpl_killdone(self, msg):
+        pass
+
+    def on_rpl_closing(self, msg):
+        pass
+
+    def on_rpl_closeend(self, msg):
+        pass
+
+    def on_rpl_endofnames(self, msg):
+        pass
+
+    def on_rpl_links(self, msg):
+        pass
+
+    def on_rpl_endoflinks(self, msg):
+        pass
+
+    def on_rpl_banlist(self, msg):
+        pass
+
+    def on_rpl_endofbanlist(self, msg):
+        pass
+
+    def on_rpl_endofwhowas(self, msg):
+        pass
+
+    def on_rpl_info(self, msg):
+        pass
+
+    def on_rpl_motd(self, msg):
+        pass
+
+    def on_rpl_infostart(self, msg):
+        pass
+
+    def on_rpl_endofinfo(self, msg):
+        pass
+
+    def on_rpl_motdstart(self, msg):
+        pass
+
+    def on_rpl_endofmotd(self, msg):
+        pass
+
+    def on_rpl_youreoper(self, msg):
+        pass
+
+    def on_rpl_rehashing(self, msg):
+        pass
+
+    def on_rpl_myportis(self, msg):
+        pass
+
+    def on_rpl_time(self, msg):
+        pass
+
+    def on_rpl_usersstart(self, msg):
+        pass
+
+    def on_rpl_users(self, msg):
+        pass
+
+    def on_rpl_endofusers(self, msg):
+        pass
+
+    def on_rpl_nousers(self, msg):
+        pass
+
+    def on_err_nosuchnick(self, msg):
+        pass
+
+    def on_err_nosuchserver(self, msg):
+        pass
+
+    def on_err_nosuchchannel(self, msg):
+        pass
+
+    def on_err_cannotsendtochan(self, msg):
+        pass
+
+    def on_err_toomanychannels(self, msg):
+        pass
+
+    def on_err_wasnosuchnick(self, msg):
+        pass
+
+    def on_err_toomanytargets(self, msg):
+        pass
+
+    def on_err_noorigin(self, msg):
+        pass
+
+    def on_err_norecipient(self, msg):
+        pass
+
+    def on_err_notexttosend(self, msg):
+        pass
+
+    def on_err_notoplevel(self, msg):
+        pass
+
+    def on_err_wildtoplevel(self, msg):
+        pass
+
+    def on_err_unknowncommand(self, msg):
+        pass
+
+    def on_err_nomotd(self, msg):
+        pass
+
+    def on_err_noadmininfo(self, msg):
+        pass
+
+    def on_err_fileerror(self, msg):
+        pass
+
+    def on_err_nonicknamegiven(self, msg):
+        pass
+
+    def on_err_erroneusnickname(self, msg):
+        pass
+
+    def on_err_nicknameinuse(self, msg):
+        pass
+
+    def on_err_nickcollision(self, msg):
+        pass
+
+    def on_err_usernotinchannel(self, msg):
+        pass
+
+    def on_err_notonchannel(self, msg):
+        pass
+
+    def on_err_useronchannel(self, msg):
+        pass
+
+    def on_err_nologin(self, msg):
+        pass
+
+    def on_err_summondisabled(self, msg):
+        pass
+
+    def on_err_usersdisabled(self, msg):
+        pass
+
+    def on_err_notregistered(self, msg):
+        pass
+
+    def on_err_needmoreparams(self, msg):
+        pass
+
+    def on_err_alreadyregistred(self, msg):
+        pass
+
+    def on_err_nopermforhost(self, msg):
+        pass
+
+    def on_err_passwdmismatch(self, msg):
+        pass
+
+    def on_err_yourebannedcreep(self, msg):
+        pass
+
+    def on_err_youwillbebanned(self, msg):
+        pass
+
+    def on_err_keyset(self, msg):
+        pass
+
+    def on_err_channelisfull(self, msg):
+        pass
+
+    def on_err_unknownmode(self, msg):
+        pass
+
+    def on_err_inviteonlychan(self, msg):
+        pass
+
+    def on_err_bannedfromchan(self, msg):
+        pass
+
+    def on_err_badchannelkey(self, msg):
+        pass
+
+    def on_err_badchanmask(self, msg):
+        pass
+
+    def on_err_noprivileges(self, msg):
+        pass
+
+    def on_err_chanoprivsneeded(self, msg):
+        pass
+
+    def on_err_cantkillserver(self, msg):
+        pass
+
+    def on_err_nooperhost(self, msg):
+        pass
+
+    def on_err_noservicehost(self, msg):
+        pass
+
+    def on_err_umodeunknownflag(self, msg):
+        pass
+
+    def on_err_usersdontmatch(self, msg):
         pass
 
 
