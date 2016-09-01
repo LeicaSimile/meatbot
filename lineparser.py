bc0f77a1abba11f37384b55fc27972fec952e74d
Tum <tummytum3@gmail.com>
2016-08-31 20:00:09 -0400

    Import division from __future__ to make quotients floats by default

--- lineparser.py
+++ lineparser.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import division, unicode_literals
 try:
     import ConfigParser as configparser  # Python 2
 except ImportError:
